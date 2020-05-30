a4cd3f7f7052ce8c1e1205f3b5ff9f6bda4a4309
Pere <pere.solaclaver@gmail.com>
2020-05-30 22:06:48 +0100

    Add env variables and local host capabilities

--- src/api.js
+++ src/api.js
@@ -1,11 +1,15 @@
+// dependencies
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
+require('dotenv').config();
 
+// instantiate express
 const app = express();
 
 const router = express.Router();
 
+// middleware
 app.use(cors());
 
 router.get('/', (req, res) => {
@@ -18,5 +22,12 @@ router.get('/test', (req, res) => {
 
 app.use(`/.netlify/functions/api`, router);
 
-module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports = app;
+if (process.env.NODE_ENV === 'development') {
+	const port = process.env.PORT;
+	app.listen(port, () => {
+		console.log(`\n\n*** Server running on port ${port} ***\n\n`);
+	});
+} else {
+	module.exports.handler = serverless(app);
+}
